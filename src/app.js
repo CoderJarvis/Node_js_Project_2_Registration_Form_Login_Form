3c697e282f657d869b29404e1f222f8d01649fba
saswat <saswatkumarpanda515@gmail.com>
2022-02-11 23:16:45 +0530

    better

--- src/app.js
+++ src/app.js
@@ -5,100 +5,96 @@ var app = express()
 require('./db/conn.js');    //database connection 
 const { People } = require('./models/schema');  //our collection
 
-const staticPath=path.join(__dirname,'../public')
+const staticPath = path.join(__dirname, '../public')
 app.use(express.static(staticPath));   //for static file serving
 
-const partialPath=path.join(__dirname,'../templates/partials'); 
-const viewPath=path.join(__dirname,'../templates/views');       
+const partialPath = path.join(__dirname, '../templates/partials');
+const viewPath = path.join(__dirname, '../templates/views');
 
-app.set('view engine','hbs');   //for template engines
-app.set('views',viewPath);      //for view folder
+app.set('view engine', 'hbs');   //for template engines
+app.set('views', viewPath);      //for view folder
 hbs.registerPartials(partialPath);  //for partial folder
-
 
 app.use(express.urlencoded());  //for form
 
-
-app.get('/',  (req, res)=> {
+app.get('/', (req, res) => {
     res.render('index.hbs');
 })
-app.get('/home',  (req, res)=> {
+app.get('/home', (req, res) => {
     res.render('index.hbs');
 })
-app.get('/register',  (req, res)=> {
-  res.render('register.hbs')
+app.get('/register', (req, res) => {
+    res.render('register.hbs')
 })
 
-//for /register post request
-app.post('/register',async (req, res)=> {
+//for register post request
+app.post('/register', async (req, res) => {
 
     try {
         password = req.body.password;
         confirmPassword = req.body.confirmPassword;
-        if(password != confirmPassword)
-        {
-            res.status(404).render('showMsg.hbs',{
-                msg : "Password Didn't Matched"
+        if (password != confirmPassword) {
+            res.status(404).render('showMsg.hbs', {
+                msg: "Password Didn't Matched"
             })
         }
-        else
-        {
+        else {
             const doc = new People({
-                name : req.body.name,
-                email :  req.body.email,
-                phone : req.body.phone,
-                college : req.body.college,
-                date_of_birth : req.body.dob ,
-                password : req.body.password,
-                confirmPassword : req.body.confirmPassword
+                name: req.body.name,
+                email: req.body.email,
+                phone: req.body.phone,
+                college: req.body.college,
+                date_of_birth: req.body.dob,
+                password: req.body.password,
+                confirmPassword: req.body.confirmPassword
             })
             const result = await doc.save();
             // console.log(result);
-            res.status(201).render("showMsg.hbs",{
-                msg : "Account Successfully Created"
+            res.status(201).render("showMsg.hbs", {
+                msg: "Account Successfully Created"
             });
         }
-        
+
     } catch (error) {
-            res.status(404).render('showMsg.hbs',{
-            msg : "Some Unexpected Error Occured"
+        res.status(404).render('showMsg.hbs', {
+            msg: "Some Unexpected Error Occured"
         })
     }
 })
 
-app.get('/login',  (req, res)=> {
-  res.render('login.hbs')
+app.get('/login', (req, res) => {
+    res.render('login.hbs')
 })
 
-//for /login post request
-app.post('/login', async (req, res)=> {
-    try {  
-         const email = req.body.email;
-         const inputPassword = req.body.password;
+//for login post request
+app.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const inputPassword = req.body.password;
         //  console.log(email,inputPassword);
 
-         const data = await People.findOne({email:email})
-         const orgPassword = data.password;
+        const data = await People.findOne({ email: email })
+        const orgPassword = data.password;
         //  console.log(orgPassword);
-        if(inputPassword === orgPassword)
-        {
-            res.status(200).render('showMsg.hbs',{
-                msg : "Login Successful"
+        if (inputPassword === orgPassword) {
+            res.status(200).render('showMsg.hbs', {
+                msg: "Login Successful"
             })
         }
-        else{
-                res.status(404).render('showMsg.hbs',{
-                msg : "Username or Password Incorrect"
+        else {
+            res.status(404).render('showMsg.hbs', {
+                msg: "Username or Password Incorrect"
             })
         }
-        
+
     } catch (error) {
-            res.status(404).render('showMsg.hbs',{
-            msg : "Username or Password Incorrect"
+        res.status(404).render('showMsg.hbs', {
+            msg: "Username or Password Incorrect"
         })
     }
 })
+
 const port = process.env.PORT || 3000;
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Server running at ${port}`);
 });
--- src/models/schema.js
+++ src/models/schema.js
@@ -2,28 +2,28 @@ const mongoose = require('mongoose');
 
 //creating the schema
 const peopleSchema = new mongoose.Schema({
-    name : String,
-    email : {
-        type : String,
-        unique : true
-    } ,
-    phone : {
-        type : String,
-        unique : true
+    name: String,
+    email: {
+        type: String,
+        unique: true
     },
-    college : String,
-    date_of_birth : String,
-    password : String,
-    confirmPassword : String,
-    dateOfCreation : {
-        type : Date,
-        default : Date.now()
+    phone: {
+        type: String,
+        unique: true
+    },
+    college: String,
+    date_of_birth: String,
+    password: String,
+    confirmPassword: String,
+    dateOfCreation: {
+        type: Date,
+        default: Date.now()
     }
 })
 
 // creating our collection
-const People = mongoose.model('People',peopleSchema);
+const People = mongoose.model('People', peopleSchema);
 
 module.exports = {
-    People : People
+    People: People
 }
